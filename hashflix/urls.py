d3d1e8e446c35b5974742a0e06b2bf6604bdfabf
Ricardo Botelho <ricardo_patro@hotmail.com>
2023-08-23 16:58:52 -0300

    Configuração dos arquivos static e media

--- hashflix/urls.py
+++ hashflix/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('/', include('filme.urls')),
 ]
+
+# Usar as imagens estaticas (abrir em uma nova guia do navegador)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#Fazer upload das imagens
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
